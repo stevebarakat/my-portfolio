e31b1acfe0d62fcd5384c92dfdb628866fde5067
Steve Barakat <stevebarakat@gmail.com>
2020-06-25 02:07:04 -0400

    removed gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,6 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`
