14b74e0cf4ada376d65bafed816074da5e975b50
Steve Barakat <stevebarakat@gmail.com>
2020-07-02 01:17:54 -0400

    added transitions

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Header from "../Header/Header";
+import { motion } from 'framer-motion';
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../../scss/_style.scss';
 
- function Layout(props){
-  return (
-    <div className="mi-wrapper">
-      <Header />
-      {props.children}
-    </div>
-  );
+const item = {
+  hidden: {
+    opacity: 0,
+    // x: 400,
+  },
+  visible: {
+    opacity: 1,
+    // x: 0,
+  },
 }
 
+const Layout = ({ children }) => (
+  <>
+    <Header />
+    <div className="mi-wrapper">
+      <motion.main
+        variants={item}
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 1.3 }}
+      >
+        {children}
+      </motion.main>
+    </div>
+  </>
+)
 export default Layout;
